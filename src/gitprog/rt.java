c0e5f4728fe33013163aaef19289e8f60ecc0e12
crystallizes <32378198+crystallizes@users.noreply.github.com>
2019-05-23 08:50:12 +0800

    Update rt.java

--- src/gitprog/rt.java
+++ src/gitprog/rt.java
@@ -4,7 +4,6 @@ public class rt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("new");
 		System.out.println("new1");
 		System.out.println("new2");
 	}
